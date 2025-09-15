5cb6c26ab93715e952faf5b04710a593f5098615
Sebastian <sevarcas@gmail.com>
2025-09-14 22:47:15 -0500

    fixed json

--- shared/src/main/java/model/Book.java
+++ shared/src/main/java/model/Book.java
@@ -1,7 +1,10 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.io.Serializable;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Book implements Serializable {
     private String isbn;
     private String title;
